1b18f2aae7cd41984576a005a18b7d99d74e0232
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-02-22 09:19:17 -0500

    Update inital.sh

--- inital.sh
+++ inital.sh
@@ -6,7 +6,7 @@ mkdir ./chasm
 wget -c "https://www.moddb.com/downloads/mirror/206588/114/eb35009dcf17366ea9b7559f5f80e27c/?referer=https%3A%2F%2Fwww.gamepressure.com%2F" -O ./chasm/chasm.zip &&
 unzip ./chasm/chasm.zip -d ./chasm/ &&
 cp chasm-linux/run_chasm.sh ./chasm/ &&
-cp scripts/Chasm/DBox/chasmadd.conf ./chasm/Chasm/DBox/ &&
+cp chasm-linux/chasmadd.conf ./chasm/Chasm/DBox/ &&
 cp ./chasm/Chasm/music/Chasm.cue ./chasm/Chasm/ &&
 sudo ln -fn ./chasm/run_chasm.sh  /usr/bin/Chasm &&
 sudo chmod +x ./chasm/run_chasm.sh
