2070016ad8d1c1dc76030f2ec3836ca22125294f
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-02-22 07:28:35 -0500

    initial run
    initial script to run chasm

--- inital.sh
+++ inital.sh
@@ -3,6 +3,8 @@
 #sudo mkdir --parents /usr/share/games/chasm; sudo mv ~/./Chasm/Chasm/ $_
 #rm -r /usr/share/games/Chasm/
 mkdir ./chasm
+wget -c "https://www.moddb.com/downloads/mirror/206588/114/eb35009dcf17366ea9b7559f5f80e27c/?referer=https%3A%2F%2Fwww.gamepressure.com%2F" -O ./chasm/chasm.zip &&
+unzip ./chasm/chasm.zip -d ./chasm/ &&
 cp -r ./scripts/Chasm/ ./chasm/ &&
 sudo ln -fn ./chasm/Chasm/run_chasm.sh  /usr/bin/Chasm &&
 sudo chmod +x ./chasm/Chasm/run_chasm.sh
