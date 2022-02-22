91853cf42f3ebb007eb1280ff039312d4b5a55d1
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-02-22 09:23:23 -0500

    Update inital.sh

--- inital.sh
+++ inital.sh
@@ -3,7 +3,7 @@
 #sudo mkdir --parents /usr/share/games/chasm; sudo mv ~/./Chasm/Chasm/ $_
 #rm -r /usr/share/games/Chasm/
 mkdir ./chasm
-wget -c "https://www.moddb.com/downloads/mirror/206588/114/eb35009dcf17366ea9b7559f5f80e27c/?referer=https%3A%2F%2Fwww.gamepressure.com%2F" -O ./chasm/chasm.zip &&
+wget -c "https://www.moddb.com/downloads/mirror/206588/114/328b2055b493686a02c8be5cc64da67d" -O ./chasm/chasm.zip &&
 unzip ./chasm/chasm.zip -d ./chasm/ &&
 cp chasm-linux/run_chasm.sh ./chasm/ &&
 cp chasm-linux/chasmadd.conf ./chasm/Chasm/DBox/ &&
