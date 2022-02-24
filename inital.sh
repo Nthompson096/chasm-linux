3d69f954bcc6edd1096fa6e333258d6f401cf83a
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-02-23 19:23:13 -0500

    Update inital.sh

--- inital.sh
+++ inital.sh
@@ -2,12 +2,12 @@
 
 #sudo mkdir --parents /usr/share/games/chasm; sudo mv ~/./Chasm/Chasm/ $_
 #rm -r /usr/share/games/Chasm/
-mkdir ./chasm &&
-gdown --fuzzy https://drive.google.com/file/d/1n_HepDIrfmF04lH5jykpJRJXi-ti6I_2/view -O ./chasm/chasm.zip &&
+mkdir ./chasm |
+gdown --fuzzy https://drive.google.com/file/d/1n_HepDIrfmF04lH5jykpJRJXi-ti6I_2/view --continue -O ./chasm/chasm.zip &&
 unzip ./chasm/chasm.zip -d ./chasm/ &&
 rm -r ./chasm/Chasm/music &&
 mkdir ./chasm/Chasm/music &&
-wget "https://ia902806.us.archive.org/13/items/ChasmTheRiftUSA/Chasm%20-%20The%20Rift%20%28USA%29.zip" -O ./chasm/chasm-ost.zip &&
+wget -C "https://ia902806.us.archive.org/13/items/ChasmTheRiftUSA/Chasm%20-%20The%20Rift%20%28USA%29.zip" -O ./chasm/chasm-ost.zip &&
 unzip ./chasm/chasm-ost.zip -d ./chasm/Chasm/music &&
 cp chasm-linux/run_chasm.sh ./chasm/ &&
 cp chasm-linux/chasmadd.conf ./chasm/Chasm/DBox/ &&
