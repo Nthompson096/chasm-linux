6392f90b5fda727cfbc9b2c626e3c26069f9990f
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-02-23 22:14:44 -0500

    Update inital.sh

--- inital.sh
+++ inital.sh
@@ -8,6 +8,10 @@ mkdir ./chasm/Chasm/music |
 wget -c "https://ia902806.us.archive.org/13/items/ChasmTheRiftUSA/Chasm%20-%20The%20Rift%20%28USA%29.zip" -O ./chasm/chasm-ost.zip &&
 unzip ./chasm/chasm-ost.zip -d ./chasm/Chasm/music &&
 cp chasm-linux/run_chasm.sh ./chasm/ &&
+cp chasm-linux/run_addon.sh ./chasm/ &&
+cp chasm-linux/addon.conf ./chasm/Chasm/DBox/ &&
 cp chasm-linux/chasm.conf ./chasm/Chasm/DBox/ &&
 sudo ln -fn ./chasm/run_chasm.sh  /usr/bin/Chasm &&
+sudo ln -fn ./chasm/run_addon.sh  /usr/bin/Chasm-Addon &&
+sudo chmod +x ./chasm/run_addon.sh &&
 sudo chmod +x ./chasm/run_chasm.sh
