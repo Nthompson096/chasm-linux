0f127a452845007c6547c311bb8207f7ad4901bc
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-03-20 17:07:04 -0400

    Add files via upload

--- inital.sh
+++ inital.sh
@@ -2,7 +2,7 @@
 
 mkdir ~/chasm 2>/dev/null |
 gdown --fuzzy https://drive.google.com/file/d/1n_HepDIrfmF04lH5jykpJRJXi-ti6I_2/view --continue -O ~/chasm/chasm.zip &&
-unzip ~/chasm/chasm.zip -d ~/chasm/ &&
+unzip ~/chasm/chasm.zip -d ~/chasm/ -x "Chasm/*.SAV" &&
 mkdir ~/chasm/Chasm/music 2>/dev/null |
 wget -c "https://ia902806.us.archive.org/13/items/ChasmTheRiftUSA/Chasm%20-%20The%20Rift%20%28USA%29.zip" -O ~/chasm/chasm-ost.zip &&
 unzip ~/chasm/chasm-ost.zip -d ~/chasm/Chasm/music &&
