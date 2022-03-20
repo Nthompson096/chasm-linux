95cf3ca5420514e44d9f08c924296c7ee843c2a1
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-03-20 17:40:26 -0400

    Update inital.sh

--- inital.sh
+++ inital.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir ~/chasm 2>/dev/null |
-unzip ~/chasm/Chasm_Portable.zip -d ~/chasm/ -x "Chasm/*.SAV" &&
+unzip ./Chasm_Portable.zip -d ~/chasm/ -x "Chasm/*.SAV" &&
 mkdir ~/chasm/Chasm/music 2>/dev/null |
 wget -c "https://ia902806.us.archive.org/13/items/ChasmTheRiftUSA/Chasm%20-%20The%20Rift%20%28USA%29.zip" -O ~/chasm/chasm-ost.zip &&
 unzip ~/chasm/chasm-ost.zip -d ~/chasm/Chasm/music &&
