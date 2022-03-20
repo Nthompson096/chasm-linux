e4d4b473b9d2770a078550a687585fac836d0713
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-03-20 17:59:32 -0400

    Update remove-chasm.sh

--- remove-chasm.sh
+++ remove-chasm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Says on the tin of the file, will remove the chasm folder. be sure to run sudo.
-  rm -r ../chasm |
-	  rm /usr/bin/Chasm |
-	  rm /usr/bin/Chasm-Addon
+  rm -r ../chasm 2>/dev/null |
+	  rm /usr/bin/Chasm 2>/dev/null |
+	  rm /usr/bin/Chasm-Addon 2>/dev/null
