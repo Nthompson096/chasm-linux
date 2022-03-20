95a50e9a0842c15271060bdc176fd1b60fefc80c
Nothin <68661602+Nthompson096@users.noreply.github.com>
2022-03-20 17:49:31 -0400

    Update inital.sh

--- inital.sh
+++ inital.sh
@@ -6,6 +6,7 @@ unzip ./Chasm_Portable.zip -d ~/chasm/ -x "Chasm/*.SAV" &&
 mkdir ~/chasm/Chasm/music 2>/dev/null |
 wget -c "https://ia902806.us.archive.org/13/items/ChasmTheRiftUSA/Chasm%20-%20The%20Rift%20%28USA%29.zip" -O ~/chasm/chasm-ost.zip &&
 unzip ~/chasm/chasm-ost.zip -d ~/chasm/Chasm/music &&
+# Copy paste from the git folder to the home folder of the user, much much safer this way.
 cp run_chasm.sh ~/chasm/ |
 cp run_addon.sh ~/chasm/ |
 cp addon.conf ~/chasm/Chasm/DBox/ |
